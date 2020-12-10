c00d55d01de563f26617e047f1624132940206fa
Raminov228 <raminov2015@litsey2.ru>
2020-12-11 02:25:01 +0300

    забыл залить остальное

--- CyberPunk.py
+++ CyberPunk.py
@@ -1,141 +1,104 @@
+import pygame
+from pygame.draw import *
+
+#импортим все объекты
+from objects import *
+
 import os
+ 
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-#просто вынес константы в отдельный файл
-from settings import *
+pygame.init()
 
-
-class Objects():
-	'''
-	@ Абстрактный класс. У всех объектов должны быть определены координаты.
-	'''
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def get_coords():
-		return self.x, self.y
-
-
-class StaticObjects(Objects):
-	'''
-	@ Класс статичных объектов. Нужен чтобы уметь различать мобов/героев от стен и прочих объектов
-	'''
-	pass
-
-
-class LiveObjects(Objects):
-	'''
-	@ Класс мобов/героев. Нужен чтобы уметь различать мобов/героев от стен и прочих объектов
-	'''
-	pass
-
-
-class Space():
-	'''
-	@ Класс прстранства. Нужен чтобы реализовывать взаимодействие live_objects и static_objects
-	'''
-	def __init__(self, nps, mymap):
-		self.nps = nps
-		self.map = mymap
-
-	def get_hero(self):
-		for live in self.nps:
-			if type(live) == type(Hero(0, 0)):
-				return live
-
-
-class Map():
-	'''
-	@ Класс карты, просто хранит static_objects. Нужен чтобы легко проверять на коллизии
-	'''
-	def __init__(self, static_objects):
-		self.static_objects = static_objects
-
-
-class Wall(StaticObjects):
-	'''
-	@ Класс стен
-	'''
-	def is_in(self, x, y):
-		return (self.x <= x <= self.x + CELL_SIZE) and (self.y <= y <= self.y + CELL_SIZE)
-
-
-class Hero(LiveObjects):
-	'''
-	@ Класс ГГ
-	'''
-	pass
-
-
-class LevelGenerator():
-	'''
-	@ Класс генератора уровней. Нужен чтоб брать на вход номер уровня и создавать Space().
-	@ Карты хранятся в текстовом формате
-	'''
-	def __init__(self):
-		self.legend = self.get_legend()
-
-	def get_legend(self):
-		legend = dict()
-		data = self._get_data_from('legend')
-		
-		for line in data:
-			char = line.split()[0]
-			object_name = line.split()[-1]
-			legend[char] = object_name
-
-		return legend
-
-	def generate_level(self, num_of_level):
-		live_objects = []
-		static_objects = []
-		
-		name_of_file = 'level' + str(num_of_level)
-		raw_map = self._get_data_from(name_of_file)
-
-		for y in range(len(raw_map)):
-			for x in range(len(raw_map[0])):
-				coords = (x * CELL_SIZE, y * CELL_SIZE)
-
-				ch = raw_map[y][x]
-				obj_name = self.legend[ch][:-2]
-
-				if obj_name == 'Empty':
-					continue
-
-				tmp = []
-				eval('tmp.append(' + obj_name + '(' + ','.join([str(coords[0]), str(coords[1])]) + '))') 
-				tmp = tmp[0]
-
-				if self._isLive(tmp):
-					live_objects.append(tmp)
-				else:
-					static_objects.append(tmp)
-
-		return Space(live_objects, Map(static_objects))
-
-	def _isLive(self, obj):
-		objectType = type(obj)
-		return 'LiveObjects' in str(objectType.__bases__)
-
-	def _get_data_from(self, name_of_file):
-		path = os.path.join(PATH_TO_LEVELS, name_of_file)
-		if not path.endswith('.txt'):
-			path += '.txt' 
-		
-		data = []
-		with open(path, 'r') as f:
-			data = [line.strip() for line in f]
-
-		return data
-
+FPS = 30
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 generator = LevelGenerator()
 space = generator.generate_level(1)
-
-print(space.nps)
-print(space.map)
-
 hero = space.get_hero()
-print(hero.x)
-print(hero.y)
+hero.set_angle(math.pi / 2)
+
+cam = Camera(space)
+screen.blit(cam.render(), (0, 0))
+
+procc = EventProcessor(space)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+pygame.mouse.set_pos([WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2])
+colibrarion = False
+
+
+while not finished:
+	clock.tick(FPS)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			finished = True
+		
+		if event.type == pygame.MOUSEMOTION:
+			if colibrarion:
+				procc.mousemotion(event.pos)
+				colibrarion = False
+			else:
+				colibrarion = True
+			
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
+				procc.forward()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_s:
+				procc.backward()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_d:
+				procc.right()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				procc.left()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_w:
+				procc.stop()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_s:
+				procc.stop()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_d:
+				procc.stop()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				procc.stop()
+
+		if event.type == pygame.KEYDOWN:
+			print(1)
+			if event.key == pygame.K_e:
+				procc.rotateright()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_e:
+				procc.stoprotation()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+				procc.rotateleft()
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_q:
+				procc.stoprotation()		
+
+
+	rect(screen, (0, 0, 255), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT / 2))
+	rect(screen, (0, 255, 0), (0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT / 2))
+
+	screen.blit(cam.render(), (0, 0))
+	procc.update()
+	pygame.display.update()
+
+pygame.quit()
